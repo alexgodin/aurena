6365159f920a4ce91714067c2322f9d2e358158e
alex godin <alex@alexgodin.com>
2017-06-08 11:05:42 -0400

    Update 2017-06-08T15:05:39.402Z

--- custom.js
+++ custom.js
@@ -1,34 +1,52 @@
+// This file sends info from the
+// modal to mixpanel No matter what,
+// you will need to add your own *mixpanel
+// id to this file*
+
+// If you add new fields to the form, you
+// will also need to add them to the *get all inputs*
+// section. If you want to make them required,
+// add them to the *validation section*.
+
+var mixpanel_id = "ea3908c1a72acbd65ecde40918b66104";
+
 (function(e,a){if(!a.__SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,
 0)))}}var d=a;"undefined"!==typeof f?d=a[f]=[]:f="mixpanel";d.people=d.people||[];d.toString=function(b){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);b||(a+=" (stub)");return a};d.people.toString=function(){return d.toString(1)+".people (stub)"};k="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
 for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);
-mixpanel.init("c3b32002e630a0d0ebd58357456e5f30");
+mixpanel.init(mixpanel_id);
 // PUT YOUR MIXPANEL ID RIGHT UP THERE :)
 
 $( document ).ready(function() {
-
 	$('#signup-form').submit(function(event) {
 		// prevent default browser behaviour
   		event.preventDefault();
+
 		// Grab all inputs
 		var email = $('#email').val()
 		var name = $('#name').val()
-		var dna = $('#dna').is(':checked')
 
 		if (validateForm(email, name)){
 			var distinctId = mixpanel.get_distinct_id()
 	        mixpanel.identify(distinctId)
 	        // Set user attributes for mixpanel DB
 			var response = mixpanel.people.set({
-			    "$name": name,
-			    "$email": email,
-			    "$dna": dna
+	    	"$email":email,
+        "$product-recovery":$('#recovery').is(':checked').toString(),
+        "$product-hydrate":$('#hydrate').is(':checked').toString(),
+        "$product-boosters":$('#boosters').is(':checked').toString(),
+        "$product-dna":$('#dna').is(':checked').toString()
+	    });
+			fbq('track', 'AddToCart', {
+				value: 199,
+				currency: 'USD'
 			});
 			$('#myModal').modal('hide')
 			$('#form-success').show();
 			// Show alert for three seconds then disappear.
 			setTimeout(function(){
 				$('#form-success').fadeOut();
-			}, 3000)
+				window.location = "https://iter.ly/zzf6q"
+			}, 5000)
 		} else {
 			return false
 		}
@@ -36,8 +54,7 @@ $( document ).ready(function() {
 	});
 
 	function validateForm(email, name){
-
-        // Validate format of email
+    // Validate format of email
 		if (!validateEmail(email)) {
 			if (email.length == 0) {
 				showError("Email cannot be blank")
@@ -79,7 +96,6 @@ $( document ).ready(function() {
 	function capitalizeFirstLetter(string) {
     	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 	}
-
 });
 
 $(function() {
